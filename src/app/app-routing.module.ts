894b1acc532bd5718121f33cb3aba55bfaa3c1d2
SravaniMalekar <sravani.malekar@gmail.com>
2021-05-14 16:44:30 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { SelfAnalyseComponent } from './self-analyse/self-analyse.component';
+import { TweetsComponent } from './tweets/tweets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/main-dashboard', pathMatch: "full"},
+  {path: 'main-dashboard', component: MainDashboardComponent},
+  {path: 'self-analyse', component: SelfAnalyseComponent},
+  {path: 'tweets', component:TweetsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { SelfAnalyseComponent } from './self-analyse/self-analyse.component';
+import { TweetsComponent } from './tweets/tweets.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainDashboardComponent,
+    SelfAnalyseComponent,
+    TweetsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
