70de0fdae30ffe0a50944f2c54f38552fa5e6462
SravaniMalekar <sravani.malekar@gmail.com>
2021-05-18 22:45:03 +0530

    response of modal updated

--- src/app/self-analyse/self-analyse.component.ts
+++ src/app/self-analyse/self-analyse.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { MatDialog, MatDialogConfig, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Form, NgForm } from '@angular/forms';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -10,11 +9,14 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class SelfAnalyseComponent implements OnInit {
 
+  res: any =[];
   symptoms =['Fever', 'Tiredness', 'Dry-Cough','Difficulty-in-Breathing', 'Sore-Throat', 'Body-Pains','Nasal-Congestion' ,'Runny-Nose', 'Diarrhea', 'None'];
 
   age=['0-9','10-19','20-24','25-29','60+'];
 
   contact=['Yes','No','Dont-know'];
+  result:any;
+
   constructor(private modalService:NgbModal,
     public activeModal: NgbActiveModal) { }
 
@@ -32,5 +34,42 @@ export class SelfAnalyseComponent implements OnInit {
   submitForm(form: NgForm){
     this.modalService.dismissAll();
     console.log(form.value);
+    this.result = form.value;
+    // if(this.result[this.symptoms[0]] == true){
+    //   console.log('1');
+    // }
+    this.resolve();
+    
+  }
+
+
+  resolve(){
+    for(let i=0; i< this.symptoms.length ; i++){
+      if(this.result[this.symptoms[i]] == true){
+        this.res.push(1);
+      }else{
+        this.res.push(0);
+      }
+    }
+
+    for(let j=0;j< this.age.length; j++){
+      if(this.result['age']== this.age[j]){
+        this.res.push(1);
+      }else{
+        this.res.push(0);
+      }
+    }
+
+    for(let k=0;k< this.contact.length; k++){
+      if(this.result['contact']== this.contact[k]){
+        this.res.push(1);
+      }else{
+        this.res.push(0);
+      }
+    }
+
+    this.res = [this.res];
+    console.log(this.res);
   }
 }
+
