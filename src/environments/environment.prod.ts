882a502c36a9247f04fceb6e970f90e0684bf353
SravaniMalekar <sravani.malekar@gmail.com>
2021-05-23 22:05:45 +0530

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyBaUKvmM0hlPDKq4tkVV6pingYnsb9C2bw",
+    authDomain: "self-kare.firebaseapp.com",
+    projectId: "self-kare",
+    storageBucket: "self-kare.appspot.com",
+    messagingSenderId: "219761855660",
+    appId: "1:219761855660:web:e3b044abf04269b0ce2309",
+    measurementId: "G-NJZ2BQDX3V"
+  }
 };
